73820e4f775c6c5c2d5983427801b2f3678d86c4
architaz <146524548+architaz@users.noreply.github.com>
2025-08-29 14:54:05 +0530

    Created api.js

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = {
+  user: "https://user-service-demoblog-archita.azurewebsites.net",
+  post: "https://post-service-demoblog-archita.azurewebsites.net", 
+  comment: "https://comment-service-demoblog-archita.azurewebsites.net"
+};
+
+export default API_BASE_URL;
